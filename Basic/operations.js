e6b7dd46f0e37481a598346a6e0b4797bde7c9bb
Kaushal <kaushalmaurya050@gmail.com>
2025-05-28 16:02:09 +0530

    Operations

---
+++ Basic/operations.js
@@ -0,0 +1,23 @@
+// ==================Operations================== //
+
+console.log("Addition: ", 5 + 3); // 8
+console.log("Subtraction: ", 5 - 3); // 2   
+console.log("Multiplication: ", 5 * 3); // 15
+console.log("Division: ", 5 / 3); // 1.6666666666666667 
+console.log("Modulus: ", 5 % 3); // 2
+console.log("Exponentiation: ", 5 ** 3); // 125
+// console.log("Increment: ", ++5); // 6
+// console.log("Decrement: ", --5); // 4
+console.log("Floor Division: ", Math.floor(5 / 3)); // 1
+console.log("Ceiling Division: ", Math.ceil(5 / 3)); // 2
+
+let a = "Aman";
+let b = "Kumar";
+
+let c = a + " " + b; // String Concatenation
+console.log("String Concatenation: ", c); // Aman Kumar
+
+console.log("10" + 5); // "105" (String concatenation) // Note: This is string concatenation, not numeric addition  
+console.log("10" - 5); // 5 (Numeric subtraction) //
+console.log(1 + "2"); // "12" (String concatenation) //
+console.log(1 + 2 + "3"); // "33" //Type  Coercion Method 
