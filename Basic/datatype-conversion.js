c9c8d19f5891625fcb5f266ebcd39bd222fc457d
Kaushal <kaushalmaurya050@gmail.com>
2025-05-28 13:23:58 +0530

    dataType Conversion

---
+++ Basic/datatype-conversion.js
@@ -0,0 +1,13 @@
+let A = "";
+ 
+console.log(A, typeof A); // Aman string
+B = Boolean(A);
+console.log(B, typeof B); // true boolean
+
+// if(A){
+//     A = true
+//     console.log(A, typeof A);
+// }else{
+//     A = false
+//     console.log(A, typeof A);
+// }
